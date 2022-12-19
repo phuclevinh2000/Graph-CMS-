071afad596dc983b24c9ef17354f1a65a5abce56
Phuc Le <phuclevinh2000@gmail.com>
2022-12-19 22:00:22 +0200

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+];
+
+const Home: NextPage = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <Head>
+        <title>CMS Blog</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

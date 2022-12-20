01341e88b5e418a8ca5c07994ac820d378d8bbc9
Phuc Le <phuclevinh2000@gmail.com>
2022-12-21 00:01:09 +0200

    Add tailwind global and base component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { PostWidget, PostCard, Categories } from '../components';
 
 const posts = [
   { title: 'React Testing', excerpt: 'Learn React Testing' },
@@ -16,15 +17,15 @@ const Home: NextPage = () => {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
 
         <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'></div>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>

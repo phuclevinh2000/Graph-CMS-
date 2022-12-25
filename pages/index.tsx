673b1d4addbbacc2b3fc9c55513ad891146783e7
Phuc Le <phuclevinh2000@gmail.com>
2022-12-25 18:50:20 +0200

    Creating Post card

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = ({ posts }: any) => {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post: any) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.node.title} />
           ))}
         </div>
 

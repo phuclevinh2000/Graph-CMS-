0fb6232f52aef59047e32415a47ef21644890367
Vinh Phuc Le <phuclevinh2000@gmail.com>
2023-02-06 15:07:16 +0200

    t

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = ({ posts }: any) => {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
-        <title>CMS Blog</title>
+        <title>CMS Bloggggg</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
